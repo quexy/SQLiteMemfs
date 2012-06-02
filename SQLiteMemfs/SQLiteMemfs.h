1de31f33d2f6775a4a786c462a67a6ca7703fba6
quexy <quexy@freemail.hu>
2012-06-02 15:08:42 +0200

    added xml doc comments

--- SQLiteMemfs/SQLiteMemfs.h
+++ SQLiteMemfs/SQLiteMemfs.h
@@ -9,6 +9,10 @@ namespace System
     {
         namespace SQLite
         {
+            /// <summary>
+            /// An SQLite 3 VFS to keep database files in memory.
+            /// The files can be accessed both by SQLite and any third party.
+            /// </summary>
             public ref class SQLiteMemfs sealed : public System::IDisposable
             {
             private:
@@ -16,12 +20,28 @@ namespace System
                 void Destroy(Boolean disposing);
 
             public:
+                /// <summary>
+                /// Registers the VFS in the SQLite VFS stack, or increases the reference count
+                /// </summary>
                 SQLiteMemfs(void);
 
+                /// <summary>
+                /// Decreases the VFS reference count, and removes the VFS from the SQLite VFS stack if count reached zero.
+                /// </summary>
                 ~SQLiteMemfs(void);
                 !SQLiteMemfs(void);
 
+                /// <summary>
+                /// Returns a <see cref="Stream"/> of the specified file.
+                /// </summary>
+                /// <param name="file">the name of the file</param>
+                /// <returns>Returns a <see cref="Stream"/> of the file.</returns>
                 Stream^ GetStream(String^ file);
+
+                /// <summary>
+                /// Deletes the specified file, freeing up the memory it occupied.
+                /// </summary>
+                /// <param name="file">the name of the file to delete</param>
                 void Delete(String^ file);
             };
         }
