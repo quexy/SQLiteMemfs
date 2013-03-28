eaeeaaa632e80c3e85935d42cac627c73edd3a57
quexy <quexy@freemail.hu>
2013-03-28 21:25:35 +0100

    small change to createLib script

--- dependencies/createLib.cmd
+++ dependencies/createLib.cmd
@@ -2,7 +2,8 @@
 
 if "!" == "!%~1" goto :EOF
 
+echo.   Exporting symbols from %~1n.dll to %~n1.def
 echo EXPORTS > "%~n1.def"
-for /f "usebackq tokens=4" %%i in (`dumpbin "%~n1.dll" /EXPORTS ^| find " = "`) do echo %%~i>>"%~n1.def"
+for /f "usebackq tokens=4" %%i in (`dumpbin "%~n1.dll" /EXPORTS ^| find "sqlite3"`) do echo %%~i>>"%~n1.def"
 
 lib /NOLOGO /DEF:"%~n1.def" /OUT:"%~n1.lib" /MACHINE:X86
